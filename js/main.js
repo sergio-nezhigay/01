acb7c651588eb7cc6a19401b1576ea444c93b730
Sergio Nezhigay <abc1971h@gmail.com>
2023-03-02 21:44:37 +0200

    final

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ if (sign === "*") result = number1 * number2;
 if (sign === "/") result = number1 / number2;
 
 alert(`Result of ${number1} ${sign} ${number2} is ${result}`);
+
+const numberOfCircles = +prompt("Now please, enter the number of circles...");
+
+function createRGB() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function generateCircle(r) {
+  let str = `<div style="
+    background-color:${createRGB()};
+    width:${r * 2}px;
+    height:${r * 2}px;
+    float: left;
+    border: 8px solid ${createRGB()};
+    border-radius: ${r}%"></div> `;
+  document.write(str);
+}
+
+for (let i = 0; i < numberOfCircles; i++) {
+  generateCircle(i);
+}

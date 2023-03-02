aefb770ee01826430ef72db52f31a9cd9d2d281d
Sergio Nezhigay <abc1971h@gmail.com>
2023-03-02 17:03:47 +0200

    hw2 done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-const userName = prompt("Hi! What is your name? ;)") || "unknown person";
+const number1 = +prompt("Enter 1st number...");
+const number2 = +prompt("Enter the second number...");
+const sign = prompt("Enter the sign ( + , - , * , / )...");
+let result = 0;
 
-alert(`Hello, ${userName}! How are you?`);
+if (sign === "+") result = number1 + number2;
+if (sign === "-") result = number1 - number2;
+if (sign === "*") result = number1 * number2;
+if (sign === "/") result = number1 / number2;
+
+alert(`Result of ${number1} ${sign} ${number2} is ${result}`);
